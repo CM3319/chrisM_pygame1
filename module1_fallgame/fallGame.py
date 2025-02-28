57e48784168489a062514a96236249750f86c8ce
CM3319 <cmartin319@boyslatin.net>
2025-02-28 08:42:28 -0500

    saved my user nadm my previous done work

---
+++ module1_fallgame/fallGame.py
@@ -0,0 +1,14 @@
+# this is reusable code that represents our game screen
+screen = pygame.display.set_mode(width, height)
+
+
+# this is will allow us to show the title of the game when a
+# user opens our game 
+pygame.display.set_caption("Avoid the falling Objects")
+
+
+# set frame rate 
+clock - pygame.time.Clock()
+FPS = 60 
+      You, 3 days ago * first commit
+      
