8554656db0cd75ed32617d5aa62773ebdc3e6d7d
CM3319 <cmartin319@boyslatin.net>
2025-03-13 08:46:55 -0400

    got some game codes down

--- module1_fallgame/shootinggame.py
+++ module1_fallgame/shootinggame.py
@@ -13,10 +13,38 @@ class Player():
         self.rect = self.image.get_rect()
         self.rect.center = (400,550)
 
+    def update(self):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT]:
+            self.rect.x -=5
+        if keys[pygame.K_RIGHT]:
+            self.rect.x +=5
+
+
+class Bullet():
+    def __init__(self,x,y): 
+        super(). __init__()
+        self.image = pygame.Surface((5, 10))
+        self.image.fill((255, 0, 0))
+        self.rect = self.image.get_rect()
+        self.rect.center =(x,y)
+
+    def update(self):
+        self.rect.y -=7 
+        if self.rect.bttom< 0:
+            self.kill()
+
+class Enemy():
+    def __init__ (self):
+        super(). __init__(self)
+        self.image = pygame.Surface                 
+
+
+
 running = True
 
 while running:
-    for event in pygame.event.get()
+    for event in pygame.event.get():
         if event.type == pygame.QUIT:
             runninjg = False
 
